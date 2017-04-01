0157a6a86b652a9fe8573f51ae316bbd08374395
gexy <gexinyu@qq.com>
2017-04-01 13:31:02 +0800

    cSharpTest10

--- Program.cs
+++ Program.cs
@@ -225,28 +225,53 @@ using System.Text;
 //        }
 //    }
 //}
-namespace cSarpTest9_9_1
+//namespace cSarpTest9_9_1
+//{
+//    class program
+//    {
+//        static void Main()
+//        {
+//            for(int x = 1; x < 6; x++)
+//            {
+//                switch (x)
+//                {
+//                    case 2:
+//                        Console.WriteLine("x is {0} -- In Case 2", x);
+//                        break;
+//                    case 5:
+//                        Console.WriteLine("x is {0} -- In Case 5", x);
+//                        break;
+//                    default:
+//                        Console.WriteLine("x is {0} -- In Default case", x);
+//                        break;
+//                }
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//}
+namespace cSarpTest10_1
 {
+    struct Point
+    {
+        public int X;
+        public int Y;
+    }
     class program
     {
         static void Main()
         {
-            for(int x = 1; x < 6; x++)
-            {
-                switch (x)
-                {
-                    case 2:
-                        Console.WriteLine("x is {0} -- In Case 2", x);
-                        break;
-                    case 5:
-                        Console.WriteLine("x is {0} -- In Case 5", x);
-                        break;
-                    default:
-                        Console.WriteLine("x is {0} -- In Default case", x);
-                        break;
-                }
-            }
+            Point first, second, third;
+            first.X = 10; first.Y = 10;
+            second.X = 20; second.Y = 20;
+            third.X = first.X + second.X;
+            third.Y = first.Y + first.Y;
+            Console.WriteLine("first: {0},{1}", first.X, first.Y);
+            Console.WriteLine("second: {0},{1}", second.X, second.Y);
+            Console.WriteLine("third: {0},{1}", third.X, third.Y);
+
             Console.ReadKey();
         }
     }
 }
+
