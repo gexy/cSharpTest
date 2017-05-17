7e879326163430fe23df6dddac6e92171205dd3d
gexy <gexinyu@qq.com>
2017-05-17 15:54:32 +0800

    csharptest13_13

--- Program.cs
+++ Program.cs
@@ -450,5 +450,19 @@ using System.Text;
 //    }
 //}
 namespace cSharpTest13_13
-
-
+{
+    class Progeam
+    {
+        delegate int OtherDel(int InParam);
+        static void Main()
+        {
+            OtherDel del = delegate (int x)
+              {
+                  return x + 20;
+              };
+            Console.WriteLine("{0}", del(5));
+            Console.WriteLine("{0}", del(6));
+            Console.ReadKey();
+        }
+    }
+}
