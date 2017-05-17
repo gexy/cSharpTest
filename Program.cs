182e767f912d22bb8dd50444e24e11b1be774b57
gexy <gexinyu@qq.com>
2017-05-17 16:23:09 +0800

    csharptest13_14

--- Program.cs
+++ Program.cs
@@ -449,19 +449,42 @@ using System.Text;
 //        }
 //    }
 //}
-namespace cSharpTest13_13
+//namespace cSharpTest13_13
+//{
+//    class Progeam
+//    {
+//        delegate int OtherDel(int InParam);
+//        static void Main()
+//        {
+//            OtherDel del = delegate (int x)
+//              {
+//                  return x + 20;
+//              };
+//            Console.WriteLine("{0}", del(5));
+//            Console.WriteLine("{0}", del(6));
+//            Console.ReadKey();
+//        }
+//    }
+//}
+namespace cSharpTest13_14
 {
-    class Progeam
+    delegate double MyDel(int par);
+
+    class Program
     {
-        delegate int OtherDel(int InParam);
         static void Main()
         {
-            OtherDel del = delegate (int x)
-              {
-                  return x + 20;
-              };
-            Console.WriteLine("{0}", del(5));
-            Console.WriteLine("{0}", del(6));
+            MyDel del = delegate (int x) { return x + 1; };
+            MyDel le1 = (int x) => { return x + 1; };
+            MyDel le2 = (x) => { return x + 1; };
+            MyDel le3 = x => { return x + 1; };
+            MyDel le4 = x => x + 1;
+
+            Console.WriteLine("{0}", del(12));
+            Console.WriteLine("{0}", le1(12));
+            Console.WriteLine("{0}", le2(12));
+            Console.WriteLine("{0}", le3(12));
+            Console.WriteLine("{0}", le4(12));
             Console.ReadKey();
         }
     }
